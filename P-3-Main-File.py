d9b7cb1ef302fdef1e97e6944567b1e8d69ebd60
Wajahat Khan <wajahatkhan4477@gmail.com>
2024-11-21 13:43:11 +0500

    Code updated

--- P-3-Main-File.py
+++ P-3-Main-File.py
@@ -1,12 +1,33 @@
 # Number guess game
 import random
 
-rang = int(input("Set a range by giving last number in which you guess\n"))
+while True:
+    rang = input("Set a range by giving last number in which you guess\n")
+    if rang.isdigit():
+        rang = int(rang)
+        if rang <= 0:
+            print("Please enter a number bigger than 0")
+        else:
+            break
+    else:
+        print("Please write a number")
+        continue
 
-if rang.isdigit():
-    rand_num = random.randint(0, rang)
-else:
-    print("Please write an integer")
+rand_num = random.randint(0, rang)
 
+guess = 0
 
-print(rand_num)
+while True:
+    intake = input("Guess the number: ")
+    guess += 1
+    if intake.isdigit():
+        intake = int(intake)
+        if intake == rand_num:
+            print("You guess the correct number.")
+            break
+        else:
+            print("Wrong guess")
+    else:
+        print("Please write a number")
+
+print("You guess the correct answer in ", guess, "guesses")
